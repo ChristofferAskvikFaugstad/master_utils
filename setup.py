754a45665b7197512b770b4723704608b374914c
ChristofferAskvikFaugstad <christoffer.askvik.faugstad@hotmail.com>
2023-03-02 12:54:45 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='utils',
+   version='0.1',
+   author='Christoffer Askvik Faugstad',
+   packages=['utils'])
+
+# Installed with 
+# pip install -e .
+# This makes utils avaliablee everywhere
+# To uninstall
+# pip uninstall .
+# Everything have to happen in main folder
