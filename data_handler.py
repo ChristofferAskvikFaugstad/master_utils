b5d1e86a7ab529e95bbfc21c7da9bbc3a50e93a3
ChristofferAskvikFaugstad <christoffer.askvik.faugstad@hotmail.com>
2023-03-13 14:32:36 +0100

    Added nCOHandler

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,6 @@
 from utils.vasp_xml import *
 import os
+import xml.etree.ElementTree as ET
 NI30BEST = "C:\\Users\\chris\\masteroppgave\\data\\dft-data\\single\\ni30\\song\\vasprun.xml"
 DFTSTART = "C:\\Users\\chris\\masteroppgave\\data\\dft-data"
 NEBSTART = "C:\\Users\\chris\\masteroppgave\\data\\dft-data\\neb"
@@ -22,6 +23,19 @@ def get_all_structures_path(path):
 
 def get_all_structures_relpath(relpath):
     return get_all_structures_path(os.path.join(DFTSTART, relpath, "vasprun.xml"))
+
+def get_all_structures_sparse_path(path, sparse = 5):
+    structures = []
+    mytree = ET.parse(path)
+    myroot = mytree.getroot()
+    num_calculations = len(myroot.findall("calculation"))
+    for i in range(0,num_calculations, sparse):
+        structures.append(next(read_vasp_xml(path, index=i)))
+    
+    return structures
+
+def get_all_structures_sparse_relpath(relpath, sparse = 5):
+    return get_all_structures_sparse_path(os.path.join(DFTSTART, relpath, "vasprun.xml"), sparse = sparse)
 
 
 def get_names_relfolder(folder):
@@ -55,11 +69,24 @@ def plot_energies_relfolder(folder):
 def get_energy_images(images):
     return np.array([image.get_potential_energy() for image in images])
 
+
+def make_database_relfolder(folder, db_path,template = get_Ni30_template(), sparse = 5):
+    database = Database(db_path)
+    filenames = [os.path.join(folder,name) for name in get_names_relfolder(folder)]
+    for i,name in enumerate(filenames):
+        for structure in get_all_structures_sparse_relpath(name, sparse= sparse):
+            candidate = StandardCandidate(template=template, **structure.todict())
+            candidate.calc = structure.calc
+            database.store_candidate(candidate)
+        print(f"Done {i} of {len(filenames)}")
+
+
+
 if __name__ == "__main__":
     # view(get_structures_relfolder("single\\ni30_COHs"), block = True)
 
 
-    
+    make_database_relfolder("single/ni30_COs", "CO_10.03_allrelax.db", sparse = 10)
 
     pass
     # view(get_Ni30_template(), block = True)
