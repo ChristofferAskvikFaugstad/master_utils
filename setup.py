d5a137e636e8b228221e499bffa441efbf08cbb6
ChristofferAskvikFaugstad <christoffer.askvik.faugstad@hotmail.com>
2023-03-13 15:52:00 +0100

    updated version name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='utils',
-   version='1.1',
+   version='1.12',
    author='Christoffer Askvik Faugstad',
    packages=['.'] # Directory where package is located
    )
