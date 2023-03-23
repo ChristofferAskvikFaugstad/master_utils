0222ee7f4192b4ccd910251e12cb7fed9b275831
ChristofferAskvikFaugstad <christoffer.askvik.faugstad@hotmail.com>
2023-03-23 14:19:39 +0100

    Added outcar parser

--- data_handler.py
+++ data_handler.py
@@ -9,10 +9,44 @@ NEBSTART = "C:\\Users\\chris\\masteroppgave\\data\\dft-data\\neb"
 NI30COBEST = "C:\\Users\\chris\\masteroppgave\\data\\dft-data\\single\\ni30_COs\\CO12-13-22"
 
 
+class EnergyForceCalculator:
+    def __init__(self,energy, forces) -> None:
+        self.energy = energy
+        self.forces = forces
+    def get_forces(self, Atoms):
+        return self.forces
+    def get_potential_energy(self, atoms, force_consistent = False):
+        return self.energy
+        
+    
 
 
+def read_outcar(outcar_file_path):
+    with open(outcar_file_path, 'r') as f:
+        lines = f.readlines()
+        structures = []
+        for i,line in enumerate(lines):
+            if "POSCAR:" in line:
+                elements = line.split()[1:]
+                
+            if "  direct lattice vectors " in line:
+                cell_vectors = np.array([lines[i+1+j].split()[0:3] for j in range(3)], dtype=np.float64)
 
+            if "ions per type =" in line:
+                number_of_ions = [int(num_str) for num_str in line.split()[4:]]
+                tot_number_atoms = sum(number_of_ions)
+                elements_array = []
+                for element, number in zip(elements, number_of_ions):
+                    elements_array += [element]*number
 
+            if line == " POSITION                                       TOTAL-FORCE (eV/Angst)\n":
+                positions_forces = np.zeros((tot_number_atoms,6))
+                for j in range(tot_number_atoms):
+                    positions_forces[j,:] = np.array(lines[i+j+2].split(),dtype=np.float64)
+                energy = float(lines[i+j+13].split()[-2])
+                structures.append(Atoms(elements_array, positions=positions_forces[:,:3],cell=cell_vectors, pbc = True, calculator=EnergyForceCalculator(energy,positions_forces[:,3:])))
+
+    return structures
 
 def get_Ni30_template():
     return read_vasp_xml_final(NI30BEST)
@@ -96,6 +130,9 @@ def make_database_relfolder(folder, db_path,template = None, sparse = 5):
 
 
 if __name__ == "__main__":
+    # structures = read_outcar("OUTCAR")
+    # view(structures, block = True)
+    
     pass
     # view(get_structures_relfolder("single\\ni30_COHs"), block = True)
 
